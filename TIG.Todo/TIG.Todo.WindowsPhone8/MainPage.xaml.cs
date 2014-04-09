3e6e5bb7d046665051f4c96c1e8b0e6804de4524
dannydwarren <dannydwarren@gmail.com>
2014-04-09 16:58:29 -0700

    Working on LiveTiles->Creation code is good, but need a way to create per item

--- TIG.Todo/TIG.Todo.WindowsPhone8/MainPage.xaml.cs
+++ TIG.Todo/TIG.Todo.WindowsPhone8/MainPage.xaml.cs
@@ -1,41 +1,143 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Scheduler;
 using Microsoft.Phone.Shell;
-using TIG.Todo.WindowsPhone8.Resources;
+using TIG.Todo.Common;
 
 namespace TIG.Todo.WindowsPhone8
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        // Constructor
         public MainPage()
         {
             InitializeComponent();
 
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
+            ViewModel = new TodoListViewModel();
+            DataContext = ViewModel;
+            ViewModel.TodoItems.CollectionChanged += TodoItems_CollectionChanged;
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
+        public TodoListViewModel ViewModel { get; set; }
 
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
+        private void TodoItems_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            UpdateLiveTile(false);
+        }
 
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+        private void ActivateLiveTile(object sender, EventArgs e)
+        {
+            UpdateLiveTile(true);
+        }
+
+        private void UpdateLiveTile(bool createIfNotExists)
+        {
+            TodoItem[] incompleteTodoItems = 
+                ViewModel.TodoItems.Where(item => !item.IsCompleted).ToArray();
+
+            var iconicTileData = new IconicTileData()
+            {
+                Title = "TIG Todo",
+                Count = incompleteTodoItems.Length,
+                SmallIconImage = new Uri("Assets/Tiles/IconicTileSmall.png", UriKind.Relative),
+                IconImage = new Uri("Assets/Tiles/IconicTileMediumLarge.png", UriKind.Relative),
+            };
+
+            switch (ViewModel.TodoItems.Count)
+            {
+                case 0:
+                    iconicTileData.WideContent1 = string.Empty;
+                    iconicTileData.WideContent2 = string.Empty;
+                    iconicTileData.WideContent3 = string.Empty;
+                    break;
+                case 1:
+                    iconicTileData.WideContent1 = incompleteTodoItems[0].Text;
+                    iconicTileData.WideContent2 = string.Empty;
+                    iconicTileData.WideContent3 = string.Empty;
+                    break;
+                case 2:
+                    iconicTileData.WideContent1 = incompleteTodoItems[0].Text;
+                    iconicTileData.WideContent2 = incompleteTodoItems[1].Text;
+                    iconicTileData.WideContent3 = string.Empty;
+                    break;
+                default:
+                    iconicTileData.WideContent1 = incompleteTodoItems[0].Text ?? string.Empty;
+                    iconicTileData.WideContent2 = incompleteTodoItems[1].Text ?? string.Empty;
+                    iconicTileData.WideContent3 = incompleteTodoItems[2].Text ?? string.Empty;
+                    break;
+            }
+
+
+            UpdateOrCreateTile("/", iconicTileData, createIfNotExists);
+        }
+
+        private void UpdateOrCreateTile(string tileId, ShellTileData tileData, bool createIfNotExists)
+        {
+            ShellTile tileToUpdate =
+                ShellTile.ActiveTiles.FirstOrDefault(t => t.NavigationUri.OriginalString.Contains(tileId));
+            
+            if (tileToUpdate == null && createIfNotExists)
+            {
+                //NOTE: The main tile always exists, even if it's not pinned, so we don't need to worry about reaching this code for the main tile.
+                ShellTile.Create(new Uri(string.Format("/MainPage.xaml?id={0}", tileId), UriKind.Relative),
+                                 tileData, true);
+            }
+            else if (tileToUpdate != null)
+            {
+                tileToUpdate.Update(tileData);
+            }
+        }
+
+
+        private static readonly string TILE_UPDATER_TASK_NAME = "TILE_UPDATER_TASK_NAME";
+        private void StartTileUpdaterTask()
+        {
+            PeriodicTask existingTileUpdaterTask = ScheduledActionService.Find(TILE_UPDATER_TASK_NAME) as PeriodicTask;
+            if (existingTileUpdaterTask != null)
+            {
+                try
+                {
+                    ScheduledActionService.Remove(TILE_UPDATER_TASK_NAME);
+                }
+                catch (Exception ex)
+                {
+                    //NOTE: Exceptions during removal are unimportant
+                }
+            }
+
+            PeriodicTask newTileUpdaterTask = new PeriodicTask(TILE_UPDATER_TASK_NAME)
+            {
+                Description = "Updates main tile.",
+                ExpirationTime = DateTime.Now.AddDays(14),
+            };
+
+            try
+            {
+                ScheduledActionService.Add(newTileUpdaterTask);
+
+#if(DEBUG_AGENT)
+				ScheduledActionService.LaunchForTest( TILE_UPDATER_TASK_NAME, TimeSpan.FromSeconds( 60 ) );
+#endif
+            }
+            catch (InvalidOperationException exception)
+            {
+                if (exception.Message.Contains("BNS Error: The action is disabled"))
+                {
+                    MessageBox.Show("Background agents for this application have been disabled by the user.");
+                }
+
+                if (exception.Message.Contains("BNS Error: The maximum number of ScheduledActions of this type have already been added."))
+                {
+                    // No user action required. The system prompts the user when the hard limit of periodic tasks has been reached.
+                }
+            }
+            catch (SchedulerServiceException)
+            {
+                // No user action required.
+            }
+        }
+
     }
 }
